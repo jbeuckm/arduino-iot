86411e32f9f5f764500d5b59f1f181b2e9ab8700
jbeuckm <joe@beigerecords.com>
2015-08-26 22:55:10 -0500

    affecting the pin state from angular

--- www/js/controllers/light-controller.js
+++ www/js/controllers/light-controller.js
@@ -1,13 +1,30 @@
 angular.module('arduino-iot')
     .controller('LightController', function($scope, $http){
 
-        $http.get("http://192.168.0.177").then(function(response){
+        var arduino = "http://192.168.0.177";
+
+        $http.get(arduino).then(function(response){
 
             $scope.temperature = response.data.temperature;
             $scope.humidity = response.data.humidity;
 
-        }, function(err){
-            console.error(err);
+            $scope.light = (response.data.pinD4 == 1);
+
+        }, console.error);
+
+
+        $scope.$watch('light', function(newValue, oldValue) {
+
+            $http({
+                method: 'POST',
+                url: arduino,
+                data: "pinD4="+((newValue)? "On" : "Off"),
+                headers: {
+                    'Content-Type': 'application/x-www-form-urlencoded'
+                }}).then(function(response) {
+                    console.log("post responded "+response);
+                }, console.error);
+
         });
 
     });
